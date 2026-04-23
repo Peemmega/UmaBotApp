cc057c7118f83a48dfd2e9c4eef5bd3a78727a47
Peemmega <Peemmegachan@gmail.com>
2026-04-24 03:06:38 +0700

    Update LoginPage.jsx

--- uma-dashboard-ui/src/pages/LoginPage.jsx
+++ uma-dashboard-ui/src/pages/LoginPage.jsx
@@ -1,35 +1,36 @@
 export default function LoginPage({ appBase }) {
   return (
     
-    <div className="login-page">
-        <div
-            className="dashboard-page"
-            style={{
-                backgroundImage: `url(${bgImage})`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                backgroundRepeat: "no-repeat",
-                backgroundAttachment: "fixed",
-            }}
-        ></div>
-        
-        <div className="login-card">
-            <div className="login-header">Uma Bot Dashboard</div>
+<div
+    className="dashboard-page"
+    style={{
+        backgroundImage: `url(${bgImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundAttachment: "fixed",
+    }}
+    >
+    
+        <div className="login-page">
+            <div className="login-card">
+                <div className="login-header">Uma Bot Dashboard</div>
 
-            <h1 className="login-title">
-            เข้าถึง <span>Discord Account</span> ของคุณ
-            </h1>
+                <h1 className="login-title">
+                เข้าถึง <span>Discord Account</span> ของคุณ
+                </h1>
 
-            <p className="login-subtitle">
-            เข้าสู่ระบบเพื่อดูข้อมูลโปรไฟล์ของคุณ
-            </p>
+                <p className="login-subtitle">
+                เข้าสู่ระบบเพื่อดูข้อมูลโปรไฟล์ของคุณ
+                </p>
 
-            <button
-            onClick={() => (window.location.href = `${appBase}/login`)}
-            className="login-button"
-            >
-            🎮 Connect with Discord
-            </button>
+                <button
+                onClick={() => (window.location.href = `${appBase}/login`)}
+                className="login-button"
+                >
+                🎮 Connect with Discord
+                </button>
+            </div>
         </div>
     </div>
   );
