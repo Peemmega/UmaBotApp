835cb640bf4f63a7381ffa135168bf5ae75cc4b1
Peemmega <Peemmegachan@gmail.com>
2026-04-23 21:20:06 +0700

    add Api

---
+++ services/api.ts
@@ -0,0 +1,18 @@
+const API_BASE = import.meta.env.VITE_BOT_API;
+
+export async function apiFetch(path: string, options: RequestInit = {}) {
+  const res = await fetch(`${API_BASE}${path}`, {
+    ...options,
+    headers: {
+      "Content-Type": "application/json",
+      ...(options.headers || {}),
+    },
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error(await res.text());
+  }
+
+  return res.json();
+}
