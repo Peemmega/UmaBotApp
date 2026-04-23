e57df3e38ab2f456301342309376e55742089e41
Peemmega <Peemmegachan@gmail.com>
2026-04-24 02:53:18 +0700

    Update LoginPage.jsx

--- uma-dashboard-ui/src/pages/LoginPage.jsx
+++ uma-dashboard-ui/src/pages/LoginPage.jsx
@@ -1,20 +1,24 @@
-<div className="login-page">
-  <div className="login-card">
-    <div className="login-header">UmaDnD Dashboard</div>
+export default function LoginPage({ appBase }) {
+  return (
+    <div className="login-page">
+        <div className="login-card">
+            <div className="login-header">UmaDnD Dashboard</div>
 
-    <h1 className="login-title">
-       <span>Connect your Discord Account</span>
-    </h1>
+            <h1 className="login-title">
+            Connect your <span>Discord Account</span>
+            </h1>
 
-    <p className="login-subtitle">
-      เข้าสู่ระบบเพื่อดูข้อมูลโปรไฟล์ของคุณ
-    </p>
+            <p className="login-subtitle">
+            เข้าสู่ระบบเพื่อดูข้อมูลโปรไฟล์ของคุณ
+            </p>
 
-    <button
-      onClick={() => (window.location.href = `${appBase}/login`)}
-      className="login-button"
-    >
-      🎮 Connect with Discord
-    </button>
-  </div>
-</div>
+            <button
+            onClick={() => (window.location.href = `${appBase}/login`)}
+            className="login-button"
+            >
+            🎮 Connect with Discord
+            </button>
+        </div>
+    </div>
+  );
+}
