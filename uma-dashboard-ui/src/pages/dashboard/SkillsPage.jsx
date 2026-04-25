148df5aac6bc91cc4f5c0a5dd2380ed2d91a9ba9
Peemmega <Peemmegachan@gmail.com>
2026-04-26 03:29:55 +0700

    aaaaa

--- uma-dashboard-ui/src/pages/dashboard/SkillsPage.jsx
+++ uma-dashboard-ui/src/pages/dashboard/SkillsPage.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function SkillsPage() {
   return (
-    <section className="sheet-card padding_container">
+    <section className="sheet-card">
       <div className="title-banner">
             <h2>Skills</h2>
       </div>
-      
+
       <div className="padding-content">
         <p className="page-placeholder">หน้านี้ไว้ใส่ระบบ Skills</p>
       </div>
--- uma-dashboard-ui/src/pages/dashboard/TutorialsPage.jsx
+++ uma-dashboard-ui/src/pages/dashboard/TutorialsPage.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function TutorialsPage() {
   return (
-    <section className="sheet-card padding_container">
+    <section className="sheet-card">
       <div className="title-banner">
             <h2>Tutorials</h2>
       </div>
-      
+
       <div className="padding-content">
         <p className="page-placeholder">
           หน้านี้ไว้ใส่ระบบ Tutorial
