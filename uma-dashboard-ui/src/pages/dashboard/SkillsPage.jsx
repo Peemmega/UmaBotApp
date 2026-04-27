5445042858c3eb76ac660cad5091b514772ca88c
Peemmega <Peemmegachan@gmail.com>
2026-04-27 17:07:09 +0700

    ata

--- uma-dashboard-ui/src/pages/dashboard/SkillsPage.jsx
+++ uma-dashboard-ui/src/pages/dashboard/SkillsPage.jsx
@@ -132,7 +132,7 @@ export default function SkillsPage({ userId, username }) {
           {/* <p>รายการสกิลทั้งหมดจาก UmaDnDBot</p> */}
         </div>
 
-        <div className="skills-toolbar">
+        <div className="padding-content">
           <input
             className="skills-search"
             value={search}
