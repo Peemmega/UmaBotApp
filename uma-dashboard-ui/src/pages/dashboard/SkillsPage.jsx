d2ae829c55c67bfa95fb707654cc25ef87451906
Peemmega <Peemmegachan@gmail.com>
2026-04-27 17:10:30 +0700

    Update SkillsPage.jsx

--- uma-dashboard-ui/src/pages/dashboard/SkillsPage.jsx
+++ uma-dashboard-ui/src/pages/dashboard/SkillsPage.jsx
@@ -132,7 +132,7 @@ export default function SkillsPage({ userId, username }) {
           {/* <p>รายการสกิลทั้งหมดจาก UmaDnDBot</p> */}
         </div>
 
-        <div className="padding-content">
+        <div className="skills-toolbar padding-content">
           <input
             className="skills-search"
             value={search}
