f7968b617fe2e7a87208ecd4fc0ea147877f4c3f
Peemmega <Peemmegachan@gmail.com>
2026-04-29 22:18:38 +0700

    Update ZoneEditModal.jsx

--- uma-dashboard-ui/src/components/ZoneEditModal.jsx
+++ uma-dashboard-ui/src/components/ZoneEditModal.jsx
@@ -153,7 +153,10 @@ export default function ZoneEditModal({ userId, player, zone, onClose, onSaved }
   return (
     <div className={`zone-edit-backdrop ${closing ? "closing" : ""}`} onClick={closeModal}>
       <div className={`zone-edit-modal ${closing ? "closing" : ""}`} onClick={(e) => e.stopPropagation()}>
-        <div className="zone-edit-banner">Zone Manager</div>
+        {/* <div className="zone-edit-banner">Zone Manager</div> */}
+        <div className="title-banner">
+            <h2>Zone Manager</h2>
+        </div>
 
         <div className="zone-edit-body">
           <div className="zone-edit-image-frame">
