9caff33c2c2a44a2e6a8ca73456bbdab4bdd0d89
Peemmega <Peemmegachan@gmail.com>
2026-05-09 13:49:21 +0700

    ada

---
+++ capacitor.config.js
@@ -0,0 +1,11 @@
+/** @type {import('@capacitor/cli').CapacitorConfig} */
+const config = {
+  appId: "com.peemmega.umadnd",
+  appName: "UmaDnD",
+  webDir: "uma-dashboard-ui/dist",
+  server: {
+    androidScheme: "https",
+  },
+};
+
+module.exports = config;
